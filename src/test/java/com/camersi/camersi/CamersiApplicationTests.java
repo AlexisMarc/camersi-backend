69c0956fb5ddff0fe4bc34378975281d0ea49eb5
Marcos Rincon <marcos.rincon1903@gmail.com>
2023-06-26 23:04:59 -0500

    Base

---
+++ src/test/java/com/camersi/camersi/CamersiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camersi.camersi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamersiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
